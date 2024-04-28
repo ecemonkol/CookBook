51107db6d1b9e7ef37d14e4c95d336cdafea1cb7
ecemonkol <ecemonkol@gmail.com>
2024-04-28 15:08:35 +0200

    add isVegetarian checkbox the from

--- src/Pages/RecipesPage.jsx
+++ src/Pages/RecipesPage.jsx
@@ -13,19 +13,41 @@ function RecipesPage({ recipes, setRecipes }) {
     console.log(newRecipe, "filtered");
     setRecipes(newRecipe);
   };
+
+  // function sortCalories() {
+
+  // }
+
+  // function sortName() {
+
+  // }
+
+  // function filterVegi() {
+
+  // }
+
   return (
-    <div className="recipe-cards">
-      {recipes.map((recipe) => (
-        <RecipeCard
-          id={recipe.id}
-          key={recipe.id}
-          name={recipe.name}
-          calories={recipe.calories}
-          image={recipe.image}
-          deleteItem={deleteItem}
-          //   servings={recipe.servings}
-        />
-      ))}
+    <div className="recipe-page">
+      <div className="recipes-button-container">
+        <button className="filter-button">
+          Sort by Calories (Low to High)
+        </button>
+        <button className="filter-button">Sort by Name</button>
+        <button className="filter-button">Show Only Vegetarian</button>
+      </div>
+      <div className="recipe-cards">
+        {recipes.map((recipe) => (
+          <RecipeCard
+            id={recipe.id}
+            key={recipe.id}
+            name={recipe.name}
+            calories={recipe.calories}
+            image={recipe.image}
+            deleteItem={deleteItem}
+            //   servings={recipe.servings}
+          />
+        ))}
+      </div>
     </div>
   );
 }
